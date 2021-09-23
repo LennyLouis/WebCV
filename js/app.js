bafb843674604154d8f02c29f38deb8827ddcbb9
Lenny Louis <lenny.louis35@gmail.com>
2021-09-23 11:33:50 +0200

    parallax fonctionnel

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 new fullpage('#fullpage', {
     navigation: true,
     responsiveWidth: 700,
-    anchors: ['home', 'about-us', 'contact'],
+    anchors: ['home', 'about-us'],
     parallax: true,
     scrollBar:true,
     onLeave: function(origin, destination, direction){
